3799ad56c3adda7166a7e03487f621145bcce64b
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2022-04-24 15:46:38 +0200

    search function - search entities by name

--- src/Entity.kt
+++ src/Entity.kt
@@ -18,12 +18,26 @@ class Entity(name: String, parent: Entity? = null) : EntityAbstract(name, parent
         }
     }
 
+    // Search for entity by name, TODO search by creteria
+    fun search(name: String): EntityConcrete?{
+        var entity: EntityConcrete? = null
 
+        val searchVisitor = object : Visitor {
+            override fun visit(e: EntityConcrete) {
+                if (e.name == name)
+                    entity = e
+            }
+
+            override fun visit(e: Entity): Boolean { // it can't have name to check, so we just if element is not found, if not true to continue
+                return entity == null
+            }
+        }
+        this.accept(searchVisitor)
+        return entity
+    }
 }
 
 class EntityConcrete(name: String, textBetweenEntities:String, parent: Entity? = null) : EntityAbstract(name, parent) {
-
-
     override fun accept(v: Visitor) {
         v.visit(this)
         }
--- src/Main.kt
+++ src/Main.kt
@@ -4,9 +4,10 @@
 fun main() {
     val root = Entity("name",null)
     val eSub1 = EntityConcrete("nameSub1","textBSub1",root)
-    val eSub2 = EntityConcrete("nameSub2","textBSub2",root)
-    val eSub2Sub1 = Entity("nameSub2Sub1",root)
-    val xml = Xml("header", root)
+    val eSub2 = EntityConcrete("nameSub2Sub1","textBSub2",root)
+    val eSub2Sub1 = Entity("partanInParent",root)
+    val xml = EntityConcrete("nameSub2Sub1", "textBSub2", eSub2Sub1)
+    val x = root.search("nameSub2Sub1")
     print("s");
 
 }
